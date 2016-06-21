48bcb05c0733fa763adbdfb5a315d6e753cbb7aa
Jakub Pelczar <kuba.pelczar@gmail.com>
2016-06-21 11:40:21 +0200

    Brave state title tag added

---
+++ src/directives/brave-state-title.directive.js
@@ -0,0 +1,42 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('ngBraveLayout')
+    .directive('braveStateTitle', function ($rootScope, $state, $timeout) {
+      return {
+        restrict: 'A',
+        compile: function (element, attributes) {
+          element.removeAttr('data-brave-state-title');
+
+          function setTitle(title) {
+            $timeout(function () {
+              $('html head title').text(title);
+            });
+          }
+
+          function fetchTitles(stateName, titlesList) {
+            var state = $state.get(stateName);
+            if (state && state.data && state.data.title && titlesList.indexOf(state.data.title) === -1) {
+              titlesList.push(state.data.title);
+            }
+
+            var parentName = stateName.replace(/.?\w+$/, '');
+            return parentName ? fetchTitles(parentName, titlesList) : titlesList;
+          }
+
+          function processState(state) {
+            var titles = fetchTitles(state.name, []);
+            titles.push(attributes.braveStateTitle);
+            setTitle(titles.join(' | '));
+          }
+
+          processState($state.current);
+
+          $rootScope.$on('$stateChangeStart', function (event, state) {
+            processState(state);
+          });
+        }
+      };
+    });
+}());
